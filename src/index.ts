23d622875a293405dbac309a4efc141882d9a6b1
shinyo627 <shinyo627@gmail.com>
2021-03-17 15:52:27 +0100

    Sorter now is abstract class so that child classes can call sort() on their own

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([50, 10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
-// console.log(sorter.collection);
 
-// const charactersCollection = new CharactersCollection('Greggory');
-// const sorter2 = new Sorter(charactersCollection);
-// sorter2.sort();
-// console.log('This is charactersSorter', sorter2.collection);
+// const charactersCollection = new CharactersCollection('hey');
+// charactersCollection.sort();
+// console.log('This is charactersSorter', charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -20,6 +16,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
